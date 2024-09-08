45fb6747e3c103d027326ef36b025169bbdf7529
Gudipati Devi <gudipatidevi9@gmail.com>
2024-09-08 09:30:08 +0530

    Add files via upload

---
+++ LCD_display.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  lcd.setCursor(5,0);
+  lcd.print("WELCOME");
+  lcd.setCursor(5,1);
+  lcd.print("DEVI");
+  delay(3000);
+  lcd.clear();
+  lcd.setCursor(4,0);
+  lcd.print("EMBEDDED SDC");
+  lcd.setCursor(5,1);
+  lcd.print("ARDUINO UNO");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
